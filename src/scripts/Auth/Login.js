e6e0f0a31ebaf21a971a53a85fe4af1a964369f1
Randall Thomas <randallthomasmusic@gmail.com>
2022-02-15 09:18:43 -0800

    login page HTML

--- src/scripts/Auth/Login.js
+++ src/scripts/Auth/Login.js
@@ -1,4 +1,25 @@
-//get the getUsers from DataAccess
+//get the getUsers from DataAccess 
+import {getUsers} from "../Data/DataAccess.js"
+const users = getUsers()
+// create a login form 
+
+export const LoginFormHTML = () => {
+    return `
+    <div class="login_form">
+        <form>
+            <fieldset>
+                <h2>Email:</h2>
+                <input type="email" name="email" autofocus placeholder="Email address" />
+            </fieldset>
+            <fieldset>
+                <h2>Password:</h2>
+                <input type="text" class="passsword" placeholder="Password" />
+            </fieldset>
+        </form>
+        <button id="login_button">Login</button>
+    </div>
+    `
+}
 
 //add a click event listener that lsitens for the login button slection and authenitcation
 
--- src/scripts/GiphyGram.js
+++ src/scripts/GiphyGram.js
@@ -3,6 +3,7 @@ import { PostEntryForm } from "./Feed/PostEntryForm.js"
 //create overall HTML structure and invoke each sections function that generates HTML
 export const GiphyGram = () => {
     return `
+        
         <section>
       <h1>GiphyGram</h1>
       Navbar
