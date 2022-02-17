7951d38c0a790e0030187b3cfa47ae752c449fc7
AriDaron <arielle.daron@asurion.com>
2022-02-17 11:17:52 -0600

    Users can now select recipients and create DM

--- src/scripts/GiphyGram.js
+++ src/scripts/GiphyGram.js
@@ -1,27 +1,33 @@
 import { PostEntryForm } from "./Feed/PostEntryForm.js"
-import {Post} from "./Feed/Post.js"
+import { Post } from "./Feed/Post.js"
+import { NavBarHTML } from "./Navbar.js"
+import { MessageEntryForm } from "./Messages/DirectMessageForm.js"
 //create overall HTML structure and invoke each sections function that generates HTML
 export const GiphyGram = () => {
-    return `
+  return `
         
         <section>
       <h1>GiphyGram</h1>
       Navbar
+      ${NavBarHTML()}
       </section>
     
-
+<div class="messageContainer">
       <section class="messageForm">
-        
+        ${MessageEntryForm()}
       </section>
-
+</div>
+<div>
       <section class="postForm">
         ${PostEntryForm()}
       </section>
+      </div>
      
       <section class="postFeed">
       ${Post()}
       </section>
 
+      </div>
         <footer>
 
         </footer>
--- src/scripts/Messages/DirectMessageForm.js
+++ src/scripts/Messages/DirectMessageForm.js
@@ -1,7 +1,43 @@
 //This module is responsible for generating the HTML representation for the message form.
 
-//export a function that creates the HTML elements for the message form
+import { getUsers } from "../Data/DataAccess.js"
 
+//export a function that creates the HTML elements for the message form
+export const MessageEntryForm = () => {
+    const recipient = getUsers()
+    let html = ""
+    html +=
+        `
+<div> 
+    <h3> Direct Messages</h3>
+    <section class="recipient_dropdown">
+Recipient:
+<select name="directMessage" class="message_input">
+
+${recipient.map(recipient => {
+            return `<option class="recipients" value="${recipient.id}">${recipient.name} </option>
+    `
+        }).join("")
+        }
+</select>
+</section>
+
+<section class="messageInput">
+<textarea class="messageBody" type="text" placeholder="Message to User"> </textarea>
+</section>
+<section>
+<button class="saveButton" id="saveMessage">Save</button>
+
+<button class="cancelButton" id="cancelMessage">Cancel</button>
+
+</section>
+</div>
+
+`
+    return html
+
+
+}
 //create dropdown for recipient - get users
 //don't include current logged in user
 
